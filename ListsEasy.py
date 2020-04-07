ef312471ed813e17d87855bf5b912cd9993bcb59
JulianWulf <60652604+JulianWulf@users.noreply.github.com>
2020-04-07 14:07:41 +0200

    Delete ListsEasy.py

--- ListsEasy.py
+++
@@ -1,8 +0,0 @@
-Brüder_von_J = ["Andreas", "Anton", "Valentin"]
-
-print(Brüder_von_J[0])
-print(Brüder_von_J[1])
-print(Brüder_von_J[2])
-print(Brüder_von_J[0:])
-
-#results = Pycharm just copy,paste :)
