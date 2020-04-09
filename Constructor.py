eb5b849cfbc9163b1bddf76eb7066eee716b6b58
JulianWulf <60652604+JulianWulf@users.noreply.github.com>
2020-04-09 12:20:14 +0200

    Add files via upload

---
+++ Constructor.py
@@ -0,0 +1,13 @@
+class Warum():
+    def __init__(self,f,l):
+        self.f = "Hallo Welt"
+        self.l = "Hallo Welt 2"
+    def name(self):
+        print(self.f + " " + self.l)
+
+warum1 = Warum("warUm " , " Why")
+warum1.name()
+
+warum2 = Warum("waruM " , " wHy")
+warum2.name()
+
